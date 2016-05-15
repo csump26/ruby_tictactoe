eaea315e6c7cd9ea41f8462221a4015672fec26a
Caleb Sumpter <calebsump@gmail.com>
2016-05-15 17:35:08 -0400

    Add comments

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,19 +1,29 @@
 module TicTacToe
 	
+	# List of possible winning combinations
 	WINNING_COMB = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
+	# Main class which drives the game
 	class Game
 		attr_reader :board, :current_player_id
+		# Initialize the game with a new board 
 		def initialize
+			# Ignore index 0 for easier implementation
 			@board = Array.new(10)
 			@board.each_index do |i|
 				@board[i] = i
 			end
+			# Set the current player has the first player to start the game
 			@current_player_id = 0
 		end
 
+		# Public Methods 
+
 		public
+		 
+		# Kicks off game
 		def play
+			# Ask for the player's name
 			setup_players
 
 			loop do
@@ -34,7 +44,9 @@ module TicTacToe
 
 		end
 
+		# Private methods
 		private
+		# Asks the players for their name and determins who is X or O
 		def setup_players
 
 			player_one_name = String.new
@@ -64,6 +76,7 @@ module TicTacToe
 
 		end
 
+		# Player selects available positions
 		def player_select_position(player)
 			display_board
 			selection = 0
@@ -77,7 +90,7 @@ module TicTacToe
 			@board[selection] = player.marker
 		end
 
-
+		# Shows the current board
 		def display_board
 			puts "#{@board[1]} | #{@board[2]} | #{@board[3]} "
 			puts "--+---+--"
@@ -92,9 +105,11 @@ module TicTacToe
 			@players[current_player_id]
 		end
 
+		# Returns the positions that have not been selected by either players
 		def free_positions
 			(1..9).select {|position| @board[position].is_a? Integer}
 		end
+
 
 		def switch_players!
 			@current_player_id = 1 - @current_player_id
@@ -104,6 +119,7 @@ module TicTacToe
 			free_positions.empty?
 		end
 
+		# Checks if the player has selected any of the winning combinations
 		def player_has_won?(player)
 			WINNING_COMB.any? do |win|
 				win.all? {|position| @board[position] == player.marker}
